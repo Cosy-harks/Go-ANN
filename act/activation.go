de263ff1a0993ee5de08c89cfb317fa7bb2edb74
Cody Harkins <us3rs1@gmail.com>
2021-01-20 13:51:27 -0800

    added ReLU and SoftSign for activation functions. Untestedgit add act/activation.go

--- act/activation.go
+++ act/activation.go
@@ -3,6 +3,12 @@ package act
 
 import "math"
 
+// Activation functions implemented
+// Linear   2
+// Sigmoid  3
+// ReLU     4
+// SoftSign 1
+
 // Activation provides an interface to follow for making
 // different versions of activation methods
 type Activation interface {
@@ -10,28 +16,38 @@ type Activation interface {
 	Derivative([]float64) []float64
 }
 
-// Linear implments the activation interface
+// SoftSign implements the activation interface
+// using f(x) = x/(1 + |x|)
+// It is tanh like (~-1.0?, ~+1.0?)
+type SoftSign struct{}
+
+// Act of SoftSign makes a smooth transition (-1, 1)
+func (s SoftSign) Act(x []float64) []float64 {
+	guess := make([]float64, len(x), len(x))
+	for i, x := range x {
+		guess[i] = x / (1.0 + math.Abs(x))
+	}
+	return guess
+}
+
+// Derivative of SoftSign makes a smooth transition (-1, 1)
+func (s SoftSign) Derivative(y []float64) []float64 {
+	v := make([]float64, len(y), len(y))
+	for i, y := range y {
+		v[i] = math.Pow(1.0/(1.0+math.Abs(y)), 2.0)
+	}
+	return v
+}
+
+// Linear implements the activation interface
 // using a linear function
 type Linear struct{}
-
-// Sigmoid implments the activation interface
-// using a smooth transition (0, 1)
-type Sigmoid struct{}
 
 // Act of Linear technically doesn't change the input data, but does return it.
 func (l Linear) Act(x []float64) []float64 {
 	guess := make([]float64, len(x), len(x))
 	for i, v := range x {
 		guess[i] = v
-	}
-	return guess
-}
-
-// Act on x suchthat y = 1/(1+e^-x))
-func (S Sigmoid) Act(x []float64) []float64 {
-	guess := make([]float64, len(x), len(x))
-	for i, v := range x {
-		guess[i] = (1. / (1. + math.Pow(math.E, -v)))
 	}
 	return guess
 }
@@ -45,6 +61,19 @@ func (l Linear) Derivative(x []float64) []float64 {
 	return v
 }
 
+// Sigmoid implments the activation interface
+// using a smooth transition (0, 1)
+type Sigmoid struct{}
+
+// Act on x suchthat y = 1/(1+e^-x))
+func (S Sigmoid) Act(x []float64) []float64 {
+	guess := make([]float64, len(x), len(x))
+	for i, v := range x {
+		guess[i] = (1. / (1. + math.Pow(math.E, -v)))
+	}
+	return guess
+}
+
 // Derivative on y suchthat y` = y(1. - y)
 func (S Sigmoid) Derivative(y []float64) []float64 {
 	v := make([]float64, len(y), len(y))
@@ -52,4 +81,32 @@ func (S Sigmoid) Derivative(y []float64) []float64 {
 		v[i] = y * (1. - y)
 	}
 	return v
+}
+
+// ReLU implments the activation interface
+// using a hard transition y = {0; x < 0, x; x >= 0}
+type ReLU struct{}
+
+func (r ReLU) Act(x []float64) []float64 {
+	guess := make([]float64, len(x), len(x))
+	for i, y := range x {
+		if y >= 0 {
+			guess[i] = y
+		} else {
+			guess[i] = 0.0
+		}
+	}
+	return guess
+}
+
+func (r ReLU) Derivative(y []float64) []float64 {
+	v := make([]float64, len(y), len(y))
+	for i, y := range y {
+		if y >= 0 {
+			guess[i] = 1.0
+		} else {
+			guess[i] = 0.0
+		}
+	}
+	return guess
 }
